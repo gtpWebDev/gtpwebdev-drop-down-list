cc926ab1f16b85e0a80aca9d086c41d4099a2dd6
gtpWebDev <33732289+gtpWebDev@users.noreply.github.com>
2024-03-13 08:13:55 +0000

    Add simple styling options

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 A configurable Dropdown component alternative
 
-
 This library is currently private.
 
 ## Installation
@@ -16,7 +15,7 @@ npm install gtpwebdev-drop-down-list --save
 ```javascript
 import createDropDown from 'gtpwebdev-drop-down-list';
 
-const newDropDown = createDropDown()
+const newDropDown = createDropDown();
 ```
 
 ## Configuration
@@ -26,29 +25,72 @@ const newDropDown = createDropDown()
 Add the options available to the user - a display text and stored value for each option
 
 ```javascript
-newDropDown.setList(
-[
-  {
-    text: "List item number 1",
-    value: 1
-  },
-  {
-    text: "List item number 2",
-    value: 2
-  }
-]
+newDropDown.setList([
+	{
+		text: 'List item number 1',
+		value: 1,
+	},
+	{
+		text: 'List item number 2',
+		value: 2,
+	},
+]);
 ```
 
 ### setBarBackground()
 
-Set the background colour of the top selection bar
+Set the background colour of the top selection bar. All standard CSS color formats can be used.
+
 ```javascript
-newDropDown.setBarBackground("#ffdb4f");
+newDropDown.setBarBackground('#ffdb4f');
+newDropDown.setBarBackground('rgb(90, 147, 197)');
+newDropDown.setBarBackground('hsl(0, 100%, 50%)');
+```
+
+### setFontSize()
+
+Set the text size for all elements of the drop down list. Note, all CSS standard font-size units can be used. The default is "20px"
+
+```js
+newDropDown.setFontSize('2rem');
+newDropDown.setFontSize('16px');
+newDropDown.setFontSize('1.5em');
+```
+
+### setBarFontColor()
+
+Set the font color of the top selection bar. All standard CSS color formats can be used.
+
+```js
+newDropDown.setBarFontColor('2rem');
+newDropDown.setBarFontColor('16px');
+newDropDown.setBarFontColor('1.5em');
+```
+
+### setOptionBackgroundColor()
+
+Set the background colour of the drop down options. All standard CSS color formats can be used.
+
+```js
+newDropDown.setOptionBackgroundColor('#ffdb4f');
+newDropDown.setOptionBackgroundColor('rgb(90, 147, 197)');
+newDropDown.setOptionBackgroundColor('hsl(0, 100%, 50%)');
+```
+
+### setOptionFontColor()
+
+Set the font color of the drop down options. All standard CSS color formats can be used.
+
+```js
+newDropDown.setOptionFontColor('#f8f1e4');
+newDropDown.setOptionFontColor('rgb(90, 147, 197)');
+newDropDown.setOptionFontColor('hsl(0, 100%, 50%)');
 ```
 
 ### generateDOM()
 
 Add the DOM node to a parent node
+
 ```javascript
 const newDOM = newDropDown.generateDOM();
 parentNode.appendChild(newDOM);
@@ -57,6 +99,7 @@ parentNode.appendChild(newDOM);
 ### getSelectedOption()
 
 Get an object containing the selected option text and value
+
 ```js
 const { text, value } = newDropDown.getSelectedOption();
 ```
@@ -64,15 +107,12 @@ const { text, value } = newDropDown.getSelectedOption();
 ### Collecting the text and value of the selection option
 
 A "change" event is dispatched when an item on the drop down list is selected.
-```js
-newDOM.addEventListener(
-  "change",
-  () => {
-    const { text, value } = newDropDown.getSelectedOption();
-  }
-)
-```
 
+```js
+newDOM.addEventListener('change', () => {
+	const { text, value } = newDropDown.getSelectedOption();
+});
+```
 
 ### An example
 
@@ -103,11 +143,9 @@ selectProjContainer.appendChild(newDOM);
 ```
 
 Event listener:
+
 ```js
-newDOM.addEventListener(
-  "change",
-  () => {
-    const { text, value } = newDropDown.getSelectedOption();
-  }
-)
+newDOM.addEventListener('change', () => {
+	const { text, value } = newDropDown.getSelectedOption();
+});
 ```
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import "./styles.css";
-import DownCaret from "./menu-down.svg";
+import './styles.css';
+import DownCaret from './menu-down.svg';
 
 /*
   Critical:
   - caret not showing
 
   Optional:
-  - Change text size
-  - Change text colour
   - Change drop down element background color
   - Add a default option and "defaultOption" function
   - Change 
@@ -15,108 +13,114 @@ import DownCaret from "./menu-down.svg";
 */
 
 function createDropDown() {
+	let optionsArray;
+	let displayList;
+	let selectedOption = {};
+	let optionFontColor = '#ffffff';
+	let optionBackgroundColor = 'rgb(90, 147, 197)';
 
-  let optionsArray;
-  let displayList;
-  let selectedOption = {};
+	const container = document.createElement('div');
+	const optionsContainer = document.createElement('div');
+	const topBar = document.createElement('div');
+	const topBarText = document.createElement('p');
+	const topBarCaret = document.createElement('img');
 
-  const container = document.createElement("div");
-  const optionsContainer = document.createElement("div");
-  const topBar = document.createElement("div");
-  const topBarText = document.createElement("p");
-  const topBarCaret = document.createElement("img");
+	// The "change" event will be dispatched on an option being changed
+	// This event can be listened for to collect the selected value
+	const changeEvent = new Event('change', { bubbles: true });
 
-  // The "change" event will be dispatched on an option being changed
-  // This event can be listened for to colelct the selected value
-  const changeEvent = new Event("change", {bubbles: true})
+	const getSelectedOption = () => selectedOption;
 
-  const getSelectedOption = () => selectedOption;
+	const setOptions = (options) => (optionsArray = options);
 
-  const setOptions = (options) => optionsArray = options;
+	const setFontSize = (size) =>
+		container.setAttribute('style', `font-size: ${size}`);
 
-  const setBarBackground = (colour) =>
-    topBar.setAttribute("style", `background-color:${colour}`
-  )
+	const setBarBackground = (colour) =>
+		topBar.setAttribute('style', `background-color:${colour}`);
 
-  const updateOptionsVisibility = (newVis) => {
-    displayList = newVis;
-    if (newVis) {
-      optionsContainer.setAttribute("class","drop-down-list-display");
-    } else {
-      optionsContainer.setAttribute("class","drop-down-list-no-display");
-    }
-  }
+	const setBarFontColor = (color) =>
+		topBarText.setAttribute('style', `color: ${color}`);
 
-  const generateOptionsContainer = () => {
+	const setOptionFontColor = (color) => (optionFontColor = color);
 
-    optionsArray.forEach((element,index) => {
-      
-      const option = document.createElement("div");
-      option.setAttribute("class", "selector-drop-down-item");
-      option.setAttribute("style",`top: calc(${index+1}*var(--drop-down-height))`);
-      option.setAttribute("data-selected-option-text",element.text);
-      option.setAttribute("data-selected-option-value",element.value);
-      option.textContent = element.text;
+	const setOptionBackgroundColor = (color) => (optionBackgroundColor = color);
 
-      option.addEventListener("click",() => {
-        selectedOption = {
-          text: option.dataset.selectedOptionText,
-          value: option.dataset.selectedOptionValue
-        }
-        topBarText.textContent = selectedOption.text;
-        updateOptionsVisibility(false);
-        option.dispatchEvent(changeEvent)
-      });
-      
-      optionsContainer.appendChild(option);
+	const updateOptionsVisibility = (newVis) => {
+		displayList = newVis;
+		if (newVis) {
+			optionsContainer.setAttribute('class', 'drop-down-list-display');
+		} else {
+			optionsContainer.setAttribute('class', 'drop-down-list-no-display');
+		}
+	};
 
-    });
+	const generateOptionsContainer = () => {
+		optionsArray.forEach((element, index) => {
+			const option = document.createElement('div');
+			option.setAttribute('class', 'selector-drop-down-item');
+			option.setAttribute(
+				'style',
+				`top: calc(${index + 1}*var(--drop-down-height)); color:${optionFontColor}; background-color: ${optionBackgroundColor}`
+			);
+			option.setAttribute('data-selected-option-text', element.text);
+			option.setAttribute('data-selected-option-value', element.value);
+			option.textContent = element.text;
 
-    return optionsContainer;
+			option.addEventListener('click', () => {
+				selectedOption = {
+					text: option.dataset.selectedOptionText,
+					value: option.dataset.selectedOptionValue,
+				};
+				topBarText.textContent = selectedOption.text;
+				updateOptionsVisibility(false);
+				option.dispatchEvent(changeEvent);
+			});
 
-  }
+			optionsContainer.appendChild(option);
+		});
 
+		return optionsContainer;
+	};
 
-  const generateDOM = () => {
+	const generateDOM = () => {
+		updateOptionsVisibility(false);
 
-    updateOptionsVisibility(false);
+		container.setAttribute('class', 'drop-down-container');
 
-    
-    container.setAttribute("class","drop-down-container");
-    
-    const optionsContainerDOM = generateOptionsContainer(displayList);
-    container.appendChild(optionsContainerDOM);
-   
-    topBar.setAttribute("class","selector-bar");
+		const optionsContainerDOM = generateOptionsContainer(displayList);
+		container.appendChild(optionsContainerDOM);
 
-    topBarText.setAttribute("id","selector-text");
-    topBarText.textContent = "Make a selection...";
-    topBar.appendChild(topBarText);
+		topBar.setAttribute('class', 'selector-bar');
 
-    topBarCaret.setAttribute("id","selector-caret");
-    topBarCaret.setAttribute("src",DownCaret);
-    topBar.appendChild(topBarCaret);
-   
-    container.appendChild(topBar);
+		topBarText.setAttribute('id', 'selector-text');
+		topBarText.textContent = 'Make a selection...';
+		topBar.appendChild(topBarText);
 
-    topBar.addEventListener("click",() => {
-      updateOptionsVisibility(!displayList);
-      //generateDOM()
-    });
+		topBarCaret.setAttribute('id', 'selector-caret');
+		topBarCaret.setAttribute('src', DownCaret);
+		topBar.appendChild(topBarCaret);
 
-    return container;
+		container.appendChild(topBar);
 
-  }
+		topBar.addEventListener('click', () => {
+			updateOptionsVisibility(!displayList);
+			//generateDOM()
+		});
 
-  return {
-    generateDOM,
-    setOptions,
-    getSelectedOption,
-    setBarBackground
-  };
+		return container;
+	};
 
-};
+	return {
+		generateDOM,
+		setOptions,
+		getSelectedOption,
+		setFontSize,
+		setBarBackground,
+		setBarFontColor,
+		setOptionBackgroundColor,
+		setOptionFontColor,
+	};
+}
 
-export {
-  createDropDown as default
-};
+export { createDropDown as default };
